31aad6d627f231e3e352fc103d4704d5525fed82
Paul Hobson <pmhobson@gmail.com>
2015-10-30 16:35:30 -0700

    add test for flood_area

--- tidegates/tests/test_tidegates.py
+++ tidegates/tests/test_tidegates.py
@@ -1,6 +1,32 @@
+from pkg_resources import resource_filename
+
 import nose.tools as nt
 import numpy.testing as nptest
 import tidegates.testing as tgtest
+import arcpy
 
-from tidegates import tidegates
+from tidegates import tidegates, utils
+
+
+def test_flood_area():
+    ws = resource_filename("tidegates", "testing")
+    with utils.WorkSpace(ws), utils.OverwriteState(True):
+        filename = "test_flood_area_output.shp"
+        result = tidegates.flood_area(
+            dem='test_dem.tif',
+            polygons='test_zones.shp',
+            ID_column="GeoID",
+            elevation_feet=7.8,
+            filename=filename,
+            verbose=False
+        )
+        result.saveToFile(filename)
+
+    nt.assert_true(isinstance(result, arcpy.Result))
+    tgtest.assert_shapefiles_are_close(
+        resource_filename("tidegates.testing", "known_flood_area_output.shp"),
+        resource_filename("tidegates.testing", filename)
+    )
+
+    utils.cleanup_temp_results(result)
 
